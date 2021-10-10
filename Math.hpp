6011af5ee456e99dacbced593259976f56284f61
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-10-10 08:55:36 -0300

    feat: criado método para limitar um valor a um range especificado

--- Math.hpp
+++ Math.hpp
@@ -11,13 +11,32 @@
 
 #pragma once
 
+/**
+ * @brief Classe estática para operações matemáticas recorrentes
+ * 
+ */
 class Math
 {
 private:
 
 public:
     Math() {}
-    static long map(long x, long in_min, long in_max, long out_min, long out_max){
-        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    static int map(int x, int in_min, int in_max, int out_min, int out_max, bool limit = false){
+        int out = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        if(limit){
+            return Math::limit(out, out_min, out_max);
+        }else{
+            return out;
+        }
+    }
+
+    static int limit(int in, int lim1, int lim2){
+        int lMin, lMax, out;
+        if(lim1 == lim2) return lim1;
+        lMin = lim1 < lim2 ? lim1 : lim2;
+        lMax = lim1 > lim2 ? lim1 : lim2;
+        out = min(in, lMax);
+        out = max(in, lMin);
+        return out;
     }
 };
