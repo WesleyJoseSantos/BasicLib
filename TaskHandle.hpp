28b45aba276c43273f5d183c670aee9b2cebd5d7
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-10-10 08:56:12 -0300

    doc: documentado classe

--- TaskHandle.hpp
+++ TaskHandle.hpp
@@ -13,28 +13,59 @@
 
 #include "TimerHandle.hpp"
 
+/**
+ * @brief Manipulador de task
+ * 
+ */
 class TaskHandle
 {
 private:
     TimerHandle taskTimer;
     void (*task)();
 public:
+    /**
+     * @brief Constrói um novo objeto do tipo TaskHandle
+     * 
+     */
     TaskHandle(){}
     void init(unsigned long timer, void (*task)()){
         setTimer(timer);
         setTaskCallback(task);
     }
+
+    /**
+     * @brief Seta o intervalo de execução da task
+     * 
+     * @param timer 
+     */
     void setTimer(unsigned long timer){
         taskTimer.setTimer(timer);
     }
+
+    /**
+     * @brief Seta o método a ser executado pela task
+     * 
+     * @param task 
+     */
     void setTaskCallback(void (*task)()){
         this->task = task;
     }
+
+    /**
+     * @brief Executa a task
+     * 
+     * @param timer Intervalo de execução da task
+     */
     void run(unsigned long timer){
         if(taskTimer.tick(timer)){
             this->task();
         }
     }
+
+    /**
+     * @brief Executa a task
+     * 
+     */
     void run(){
         if(taskTimer.tick()){
             this->task();
